3b53ae285092496a259da5e620f6ec3bfe8b4fab
Ashwin <ashwingairola999@gmail.com>
2021-01-31 02:34:39 +0530

    Added ConfigModule and TypeOrmModule. Database connection wired up.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-	imports: [],
+	imports: [
+		ConfigModule.forRoot(),
+		TypeOrmModule.forRoot({
+			type: 'postgres',
+			host: process.env.PORTFOLIO_DB_HOST,
+			port: +process.env.PORTFOLIO_DB_PORT,
+			database: process.env.PORTFOLIO_DB,
+			username: process.env.PORTFOLIO_DB_USER,
+			password: process.env.PORTFOLIO_DB_PASSWORD
+		})
+	],
 	controllers: [AppController],
 	providers: [AppService]
 })
